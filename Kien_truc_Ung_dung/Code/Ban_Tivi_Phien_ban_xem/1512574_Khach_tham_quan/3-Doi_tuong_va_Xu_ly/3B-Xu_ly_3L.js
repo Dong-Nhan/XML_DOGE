24b093cae03cb383e71ef2834a6230fd60bd54fb
xuantinfx <xuantinfx@hotmail.com>
2018-03-20 19:17:22 +0700

    fix bug ứng dụng cho khách

--- Kien_truc_Ung_dung/Code/Ban_Tivi_Phien_ban_xem/1512574_Khach_tham_quan/3-Doi_tuong_va_Xu_ly/3B-Xu_ly_3L.js
+++ Kien_truc_Ung_dung/Code/Ban_Tivi_Phien_ban_xem/1512574_Khach_tham_quan/3-Doi_tuong_va_Xu_ly/3B-Xu_ly_3L.js
@@ -20,23 +20,40 @@ function Tao_Th_Danh_sach_San_pham(Danh_sach) {
     var Ten = Danh_sach[i].getAttribute(TAG.Ten);
     var Ma_so = Danh_sach[i].getAttribute(TAG.Ma_so);
     var Don_gia_Ban = parseInt(Danh_sach[i].getAttribute(TAG.Don_gia_Ban));
+    var Trang_Thai_Con_Hang = Danh_sach[i].getAttribute(TAG.Trang_thai_Con_hang);
+    if(Trang_Thai_Con_Hang == 'true'){
+      Trang_Thai_Con_Hang = `Còn`;
+    }
+    else if(Trang_Thai_Con_Hang == 'false'){
+      Trang_Thai_Con_Hang = `Không`;
+    }
+    else if(Trang_Thai_Con_Hang == undefined){
+      Trang_Thai_Con_Hang = `Không rõ`;
+    }
     
     var Th_Hinh = document.createElement("img");
     Th_Hinh.src = `${Dia_chi_Media}/${Ma_so}.png`;
     Th_Hinh.style.cssText = `width: 80%`;
 
     var Th_Thong_tin = document.createElement("div");
+
     var Th_Ten = document.createElement('p');
     Th_Ten.innerText = Ten;
+
     var Th_Don_Gia_Ban = document.createElement('p');
     Th_Don_Gia_Ban.innerText = `Đơn giá Bán: ${Don_gia_Ban.toLocaleString("vi")}`;
+    
+    var Th_Trang_Thai_Con_Hang = document.createElement('p');
+    Th_Trang_Thai_Con_Hang.innerText = `Còn hàng: ${Trang_Thai_Con_Hang}`;
+
     Th_Thong_tin.appendChild(Th_Ten);
     Th_Thong_tin.appendChild(Th_Don_Gia_Ban);
 
     var Th_Mat_hang = document.createElement("li")
-    Th_Mat_hang.className='col-md-3';
+    Th_Mat_hang.className='col-md-3 col-sm-6 col-12';
     Th_Mat_hang.appendChild(Th_Hinh)
     Th_Mat_hang.appendChild(Th_Thong_tin)
+    Th_Mat_hang.appendChild(Th_Trang_Thai_Con_Hang);
 
     Th_Danh_sach.appendChild(Th_Mat_hang)
   }
